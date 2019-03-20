38057231435d6ab73dad6f2916e38be3e548c047
James Chetwood <james@jameschetwood.co.uk>
2019-03-20 14:10:41 +0000

    Init GraphQL server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('howdy')
+// import { GraphQLServer } from "graphql-yoga";
+const { GraphQLServer } = require("graphql-yoga");
+// ... or using `require()`
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || "World"}`
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
